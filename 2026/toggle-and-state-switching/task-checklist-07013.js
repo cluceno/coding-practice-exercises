8ab38caf0b9f3beb75499aae909079673b047ca7
Carvy <144061164+cluceno@users.noreply.github.com>
2026-07-14 21:30:41 -0700

    finish task checklist exercise

--- 2026/toggle-and-state-switching/task-checklist-07013.js
+++ 2026/toggle-and-state-switching/task-checklist-07013.js
@@ -11,7 +11,19 @@ and every other task is unchanged.
 
 function toggleTask(tasks, taskID) {
     let toggle = tasks.map((task) =>{
-        task.id === taskID ? {...task, task.done = !task.done} : task; 
+        return task.id === taskID ? {...task, done: !task.done} : task; 
     });
     return toggle; 
 }
+
+/* Key learning points: 
+
+1. When using the map function, I still have to return the value that we are modifying. 
+
+2. When modifying a key:value pair in an object by declaring it a second time, use the key:value formatting directly. 
+
+Otherwise, I was able to get the structure mostly right this time. The errors are identified a little quicker and this time
+it was related to syntax rather than conceptual issues. I think 2 or 3 more related exercises will lock it in. 
+
+*/ 
+
